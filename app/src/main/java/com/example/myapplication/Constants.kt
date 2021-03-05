a29151c813b8d4716bb56c001cb3d56afe1d5152
jaydip1235 <jaydipdeyalok@gmail.com>
2021-03-05 15:08:01 +0530

    Update Constants.kt

--- app/src/main/java/com/example/myapplication/Constants.kt
+++ app/src/main/java/com/example/myapplication/Constants.kt
@@ -2,6 +2,6 @@ package com.example.myapplication
 
 object Constants {
     const val BASE_URL = "http://newsapi.org/"
-    const val API_KEY = "ba272edb71ee4af2a7ab74b03411f0ff"
+    const val API_KEY = "API_KEY"
 
 }
